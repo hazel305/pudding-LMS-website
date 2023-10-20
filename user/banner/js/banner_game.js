411c516a4db5b79191705a3ba88576f09a1058ce
Hyunny <hazel413@naver.com>
2023-10-20 17:06:11 +0900

    [HOTFIX]wordle game 오류 수정 완료

--- user/banner/js/banner_game.js
+++ user/banner/js/banner_game.js
@@ -3,7 +3,6 @@ let keyidx = 0;
     let answear = 'algorithm'.toLowerCase(); //정답!!
     let gameCount = 6; //게임 횟수
     let wordDOM ='';
-
     //정답 길이에 따라 바둑판 만들기
     function addgame(){
       wordDOM='';
@@ -15,52 +14,34 @@ let keyidx = 0;
     addgame();
     $('.game_board').css({gridTemplateColumns: `repeat(${answear.length},100px)`});
     $('.game_count').text(gameCount);
-
-
-
-
     let pushtext = [];
     let pushIDX = [];
     $('body').keydown(function(e){
-
       let key = e.key; //입력한 문자열
-
       if(col <= gameCount){
-        
         if (key.match(/^[a-zA-Z]$/)) {
           // console.log('영어');
           $('.word').eq(keyidx).find('span').text(key);
           keyidx++;
-
-
           //1row 구분
           if (keyidx % answear.length == 0) {
             if(col < gameCount){
               addgame();
             }
-
-
             pushtext = [];//사용자가 입력한 1row 단어
             pushIDX = [];//사용자가 입력한 1row의 keyidx 배열
-
             $('.word').each(function(){
               let idx = $(this).index();
-
               //1row에 해당하는것들을 각 배열에 push
               if($(this).index() < col*answear.length && $(this).index() >= (col-1)*answear.length){
                 pushtext.push($('.word').eq(idx).text());
                 pushIDX.push(idx);
               }
             });
-            
-  
-            
             //입력한 값 모두 소문자
             $.each(pushtext,function(idx,val){
               var $this = $(this);
               pushtext[idx] = val.toLowerCase();
-  
-              
               if(pushtext[idx] == answear.charAt(idx)){
                 $('.word').eq(pushIDX[idx]).addClass('o');
               } else if(answear.includes(pushtext[idx])){
@@ -70,48 +51,43 @@ let keyidx = 0;
                 $('.word').eq(pushIDX[idx]).addClass('x');
                 // console.log('틀렸워');
               }
-              
             });
-  
+            function yesOrNo (){
+              if(pushtext.join('') == answear){
+                return true;
+              } else{
+                return false;
+              }
+            }
             setTimeout(() => {
-            if(pushtext.join('') == answear){
+            if(yesOrNo()){
                 alert('정답입니다!');
                 location.href = '/pudding-LMS-website/user/banner/game_winner.php';
               }
             }, 100);
-  
-            col++;
+            if(keyidx == answear.length*gameCount){
+              if(!yesOrNo()){
+                setTimeout(() => {
+                  alert('GAME OVER');
+                  location.href = '/pudding-LMS-website/user/banner/game_over.php';
+                }, 500);
+              }
+            }
+            if(col < gameCount){
+              col++;
+            }
             $('.game_title .game_col').text(col);
           }
         }//if (key.match(/^[a-zA-Z]$/))
-        
-
-
-
-
-
-
-
-
+        console.log(keyidx);
         //backspace
         if(key =='Backspace'){
-
           if(keyidx > 0){
             if (keyidx % answear.length !== 0) {
               $('.word').eq(keyidx-1).find('span').text('');
               keyidx--;
             }
           }
-        }
-        if(keyidx == answear.length*gameCount){
-          setTimeout(() => {
-            alert('GAME OVER');
-            location.href = '/pudding-LMS-website/user/banner/game_over.php';
-          }, 500);
         }//if(key =='Backspace')
-
-
-
       } //if(col <= gameCount)
-
     });//$('body').keydown
